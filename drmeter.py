eb93a5ccd21fa5f0b0c88f95a8fe0ea703f13008
Jan Willhaus <mail@janwillhaus.de>
2014-11-21 10:13:20 +0100

    Autoformat on PEP8 Errors

--- drmeter.py
+++ drmeter.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import sys, getopt
+import sys
+import getopt
 import glob
 from os.path import isfile, join, dirname, split
 from os import remove
@@ -8,18 +9,17 @@ import math
 import numpy as np
 from pysoundfile import SoundFile
 
-blocklenSec     = 3
-RMSpercentage   = 20
-NhighestPeak    = 2
+blocklenSec = 3
+RMSpercentage = 20
+NhighestPeak = 2
 
 
 def main(argv):
     recurse = False
     textout = True
 
-
     try:
-        opts, args = getopt.getopt(argv,"hr",["help","recursive"])
+        opts, args = getopt.getopt(argv, "hr", ["help", "recursive"])
     except getopt.GetoptError:
         print("drmeter.py <audiofile or path>")
         sys.exit(2)
@@ -37,7 +37,7 @@ def main(argv):
         if isfile(arg)==True:
             filelist.append(arg)
 
-    if textout==True:
+    if textout is True:
         path = dirname(filelist[0])
         textpath = join(path, "{0}_dr.txt".format(split(path)[-1]))
         try:
@@ -49,7 +49,7 @@ def main(argv):
         linewidth = 75
         dashedline = "".join(["-"[:5]] * linewidth)
 
-        if len(path) > linewidth-18:
+        if len(path) > linewidth - 18:
             path = split(path)[-1]
 
         f.write(dashedline + "\n")
@@ -63,25 +63,23 @@ def main(argv):
     for nfile in filelist:
         DR, Peak, RMS = calc_drscore(nfile)
         if idx == 0:
-            DR_all = np.zeros((len(filelist),len(DR)))
-            DR_all[idx,:] = DR
+            DR_all = np.zeros((len(filelist), len(DR)))
+            DR_all[idx, :] = DR
         else:
-            DR_all[idx,:] = DR
-        idx+=1
+            DR_all[idx, :] = DR
+        idx += 1
 
-        if textout==True:
+        if textout is True:
             f = open(textpath, "a")
             f.write(" DR{0:02.0f}  {1:+6.2f} dB    {2:+6.2f} dB   {3}\n"
                     .format(
-                            DR.mean(),
-                            10*np.log10(np.power(10,Peak/10).mean()),
-                            10*np.log10(np.power(10,RMS/10).mean()),
-                            split(nfile)[-1]))
+                        DR.mean(),
+                        10 * np.log10(np.power(10, Peak / 10).mean()),
+                        10 * np.log10(np.power(10, RMS / 10).mean()),
+                        split(nfile)[-1]))
             f.close()
 
-
-
-    if textout==True:
+    if textout is True:
         f = open(textpath, "a")
         f.write(dashedline + "\n\n")
         f.write(" Number of files:\t{0:d}\n".format(len(filelist)))
@@ -89,52 +87,54 @@ def main(argv):
         f.write("".join(["="[:5]] * linewidth) + "\n")
         f.close()
 
+    return
 
 
 def calc_drscore(filename):
 
     data = SoundFile(filename)
 
-    NblockLen   = round(blocklenSec*data.sample_rate)
-    NblockIdx   = math.ceil(data.frames/NblockLen)
-    Nsamples    = data.frames
-    Nchannels   = data.channels
+    NblockLen = round(blocklenSec * data.sample_rate)
+    NblockIdx = math.ceil(data.frames / NblockLen)
+    Nsamples = data.frames
+    Nchannels = data.channels
 
-    RMS         = np.zeros((NblockIdx,Nchannels))
-    Pk          = np.zeros((NblockIdx,Nchannels))
+    RMS = np.zeros((NblockIdx, Nchannels))
+    Pk = np.zeros((NblockIdx, Nchannels))
 
     for nn in range(NblockIdx):
-        if nn<NblockIdx:
-            curData = data[1+(nn)*NblockLen:1+(NblockLen+(nn)*NblockLen),:]
+        if nn < NblockIdx:
+            curData = data[
+                1 + (nn) * NblockLen:1 + (NblockLen + (nn) * NblockLen), :]
         else:
-            curData = data[1+(nn)*NblockLen:,:]
+            curData = data[1 + (nn) * NblockLen:, :]
 
         for cc in range(Nchannels):
-            interim = 2*(np.power(np.abs(curData[:,cc]),2))
+            interim = 2 * (np.power(np.abs(curData[:, cc]), 2))
 
-            RMS[nn,cc] = math.sqrt(interim.mean());
-            Pk[nn,cc]  = max(abs(curData[:,cc]));
+            RMS[nn, cc] = math.sqrt(interim.mean())
+            Pk[nn, cc] = max(abs(curData[:, cc]))
 
-    iUpmostBlocks = round(NblockIdx*RMSpercentage*0.01)
+    iUpmostBlocks = round(NblockIdx * RMSpercentage * 0.01)
     RMS.sort(axis=0)
     Pk.sort(axis=0)
-    RMS[:] = RMS[::-1,:]
-    Pk[:] = Pk[::-1,:]
+    RMS[:] = RMS[::-1, :]
+    Pk[:] = Pk[::-1, :]
 
-    RMS_upmost = RMS[:iUpmostBlocks,:]
-    RMS_total = np.sqrt((np.power(RMS,2)).mean(axis=0))
+    RMS_upmost = RMS[:iUpmostBlocks, :]
+    RMS_total = np.sqrt((np.power(RMS, 2)).mean(axis=0))
 
-    pre0 = np.power(RMS_upmost,2).sum(axis=0)
+    pre0 = np.power(RMS_upmost, 2).sum(axis=0)
     pre1 = np.repeat(iUpmostBlocks, Nchannels, axis=0)
-    pre2 = np.sqrt(pre0/pre1)
+    pre2 = np.sqrt(pre0 / pre1)
 
-    DR_score = Pk[NhighestPeak-1,:]/pre2
+    DR_score = Pk[NhighestPeak - 1, :] / pre2
     RMS_score = RMS_total
-    Peak_score = Pk[0,:]
+    Peak_score = Pk[0, :]
 
-    DR_score_log = 20*np.log10(DR_score);
-    RMS_score_log = 20*np.log10(RMS_score);
-    Peak_score_log = 20*np.log10(Peak_score);
+    DR_score_log = 20 * np.log10(DR_score)
+    RMS_score_log = 20 * np.log10(RMS_score)
+    Peak_score_log = 20 * np.log10(Peak_score)
 
     print()
     print("DR analysis results:")
@@ -143,7 +143,7 @@ def calc_drscore(filename):
     print()
     print("     :  ", end="")
     for n in range(Nchannels):
-        print(" Chann {0:2d}  :: ".format(n+1), end="")
+        print(" Chann {0:2d}  :: ".format(n + 1), end="")
     print()
     print("Peak :  ", end="")
     for peak in Peak_score_log:
